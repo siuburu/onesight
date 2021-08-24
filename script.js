c11ee34f1749f23c2fa7486a27fae11cec7571f9
siuburu <urubuis1@gmail.com>
2021-08-24 13:28:07 -0400

    Tags adicionadas, falta adicionar funcionalidade ao clicar tags

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ fetch(baseURL)
     .then((data) => displayData(data));
 
 //cria uma lista não ordenada dado um array
-function makeUL(array){
+function makeUL(array, data) {
     var list = document.getElementById('nameList');
-   
-    for (var i = 0; i < array.length; i++){
-         var item = document.createElement('li');
-         var a = document.createElement('a');
+    var genres;
+    for (j = 0; j < data.lenght; j++) {
+        alert(JSON.stringify(data[j].genre).split(','));
+    }
+    genres = data[1]["genre"];
+    alert(JSON.stringify(genres));
+    alert(data[1]["genre"]);
+    for (var i = 0; i < array.length; i++) {
+        var item = document.createElement('li');
+
+        var a = document.createElement('a');
         a.setAttribute('href', '#');
         item.appendChild(a);
         a.appendChild(document.createTextNode(array[i]));
@@ -23,23 +30,12 @@ function makeUL(array){
 function displayData(data) {
     const gameNames = [];
     //cria um array só com os titulos dos jogos
-    for (var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         gameNames[i] = data[i].name;
     }
-    document.getElementById('gameList').appendChild(makeUL(gameNames));
-    //cria um array só com os generos dos jogos
-    const gameGenres = [];
-    for (var i = 0; i < data.length; i++){
-        gameGenres[i] = JSON.stringify(data[i].genre);
-    }
-    const genre = gameGenres.filter(function(item, pos, self){
-    return self.indexOf(item) ==  pos;
-    })
-    
-    genre[0].replace(/[\|;\$%@"<~>[\(\)\+]/g, "");
-    
-    console.log(genre);
-    
+    document.getElementById('gameList').appendChild(makeUL(gameNames, data));
+
+
     /*
     data.forEach(element => {
         console.log(element.name);
@@ -49,16 +45,16 @@ function displayData(data) {
     */
 }
 
-function getDescription(target, data){
+function getDescription(target, data) {
     data.forEach(element => {
-        if(element.name === target.innerHTML ){
+        if (element.name === target.innerHTML) {
             var developers = document.createElement('h3');
             var genre = document.createElement('h3');
             var publishers = document.createElement('h3');
-            var releaseDates = document.createElement('h3');
             developers.appendChild(document.createTextNode("Desenvolvedora: " + element.developers));
             genre.appendChild(document.createTextNode("Gênero: " + element.genre));
             publishers.appendChild(document.createTextNode("Distribuidora: " + element.publishers));
+            alert(target.p);
             target.appendChild(developers);
             target.appendChild(genre);
             target.appendChild(publishers);
@@ -66,21 +62,61 @@ function getDescription(target, data){
     });
 }
 
-function getEventTarget(e){
+function getGenre(data) {
+    const gameGender = [];
+    //cria um array só com os titulos dos jogos
+    for (var i = 0; i < data.length; i++) {
+        gameGender[i] = data[i].genre;
+    }
+    //Converte array de generos em array 1D
+    var arr1d = [].concat(...gameGender);
+    var lower = [];
+    //transforma valores em lowercase para facilitar remoção de duplicados
+    for (var i = 0; i < arr1d.length; i++) {
+        try {
+            lower[i] = arr1d[i].toLowerCase();
+        } catch (error) {
+
+        }
+
+    }
+    //remove valores duplicados
+    let unique = [...new Set(lower)];
+    return unique;
+}
+
+function listTags(tag) {
+
+    for (var i = 0; i < tag.length; i++) {
+        var tagButton = document.createElement('button');
+        tagButton.classList.add('btn');
+        tagButton.setAttribute("onclick", `filterSelection('${tag[i]}')`)
+        tagButton.innerHTML = tag[i];
+        document.getElementById('myTags').appendChild(tagButton);
+    }
+}
+
+fetch(baseURL)
+    .then((res) => res.json())
+    .then((data) => {
+        listTags(getGenre(data));
+    })
+
+function getEventTarget(e) {
     e = e || window.event;
     return e.target || e.srcElement;
 }
 //Função de busca baseada em filtrar baseado no que é digitado no input
 function search() {
-    var input, filter, ul, li, a, i ,txtValue;
+    var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("search");
     filter = input.value.toUpperCase();
     ul = document.getElementById("nameList");
     li = ul.getElementsByTagName("li");
-    for(var i = 0; i < li.length; i++) {
+    for (var i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
-        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
@@ -89,7 +125,7 @@ function search() {
 }
 
 var ul = document.getElementById('nameList');
-ul.onclick = function(event){
+ul.onclick = function (event) {
     var target = getEventTarget(event);
     fetch(baseURL)
         .then((res) => res.json())
